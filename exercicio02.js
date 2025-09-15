046ec24bff0ed57ce0f8b0878920ede065fe7369
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-09-15 10:34:54 -0300

    Declara uma constante chamada "PI", atribui o valor de Pi, e imprime na console

---
+++ exercicio02.js
@@ -0,0 +1,9 @@
+/*
+
+Exercício 2: Declare uma constante chamada "PI", atribua o valor de Pi a ela e imprima o valor na console.
+
+*/
+
+const PI = 3.14159;
+
+console.log(`O valor de PI é ${PI}.`);
