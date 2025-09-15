a52494ebe872bfceb2270e69429022d355023098
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-09-15 11:00:22 -0300

    Declara duas variáveis, nome e sobrenome, atribui valores, concatena, e imprime na console

---
+++ exercicio04.js
@@ -0,0 +1,13 @@
+/*
+
+Exercício 4: Declare duas variáveis chamadas "nome" e "sobrenome", atribua o seu nome e sobrenome a elas. 
+Concatene-as em uma terceira variável chamada "nomeCompleto" e imprima.
+
+*/
+
+const nome = "Mariana";
+const sobrenome = "Rodrigues";
+
+const nomeCompleto = nome + " " + sobrenome;
+
+console.log(`Nome: ${nomeCompleto}.`);
