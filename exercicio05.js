338edea498961cc44f039a260a87659bec47342d
Mariana Alves Rodrigues <rodriguesma627@gmail.com>
2025-09-15 11:17:30 -0300

    Declara duas variáveis, atribui valores, soma, e imprime o resultado

---
+++ exercicio05.js
@@ -0,0 +1,11 @@
+/*
+
+Exercício 5: Declare duas variáveis e atribua valores numéricos a elas. 
+Use o operador de adição para realizar operações entre as duas variáveis. Imprima o resultado.
+
+*/
+
+const num1 = 5;
+const num2 = 10;
+
+console.log(`${num1} + ${num2} = ${num1 + num2}.`);
