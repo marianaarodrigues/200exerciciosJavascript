e1ac7f7506a6209e9da8ae2286d29f42d4db1b64
Mariana Rodrigues <rodriguesma627@gmail.com>
2025-09-22 23:35:57 -0300

    Declara duas variáveis lógicas, atribui valores booleanos, realiza operações lógicas, e imprime na console

---
+++ exercicio06.js
@@ -0,0 +1,13 @@
+/*
+
+Declare duas variáveis e atribua valores booleanos a elas. 
+Use os operadores lógicos AND, OR e NOT para realizar operações lógicas entre as variáveis. Imprima os resultados.
+
+*/
+
+const variavel1 = true;
+const variavel2 = false;
+console.log(`${variavel1} AND ${variavel2} = ${variavel1 && variavel2}`);
+console.log(`${variavel1} OR ${variavel2} = ${variavel1 || variavel2}`);
+console.log(`NOT ${variavel1} = ${!variavel1}`);
+console.log(`NOT ${variavel2} = ${!variavel2}`);
