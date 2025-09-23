63b9add90ec5d90d74ed2156ca79176d4fdc81e7
Mariana Rodrigues <rodriguesma627@gmail.com>
2025-09-22 23:43:49 -0300

    Declara três variáveis, atribui valores numéricos, compara os valores, e imprime na console

---
+++ exercicio07.js
@@ -0,0 +1,11 @@
+/*
+
+Declare três variáveis e atribua valores numéricos a elas. 
+Use operadores de comparação para comparar os valores entre as variáveis. Imprima os resultados.
+
+*/
+
+const variavel1 = 2.2;
+const variavel2 = -5;
+const variavel3 = 10;
+console.log(`${variavel2} < ${variavel1} < ${variavel3}`);
